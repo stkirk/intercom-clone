01d61dd323930112496686596832d28d500ec5fa
stkirk <shaanekirk@gmail.com>
2022-05-12 14:37:39 -0400

    prop drilled into ChatMessage, dynamically displayed message data there

--- src/components/SupportChatBot/ChatMessage.js
+++ src/components/SupportChatBot/ChatMessage.js
@@ -1,36 +1,23 @@
 import React from "react";
 
-const ChatMessage = () => {
+const ChatMessage = ({ message }) => {
   const handleSelection = (e) => {
     console.log(e.target.name, e.target.value);
   };
   return (
     <div>
-      <p>bot message</p>
-      <button
-        className="userResponsebtn"
-        name="response 1"
-        value="1"
-        onClick={(e) => handleSelection(e)}
-      >
-        user response 1
-      </button>
-      <button
-        className="userResponsebtn"
-        name="response 2"
-        value="2"
-        onClick={(e) => handleSelection(e)}
-      >
-        user response 2
-      </button>
-      <button
-        className="userResponsebtn"
-        name="response 3"
-        value="3"
-        onClick={(e) => handleSelection(e)}
-      >
-        user response 3
-      </button>
+      <p className="botMessage">{message.text}</p>
+      {message.responseOptions.map((option) => (
+        <button
+          key={option.text}
+          className="userResponse"
+          name={option.name}
+          value={option.value}
+          onClick={handleSelection}
+        >
+          {option.text}
+        </button>
+      ))}
     </div>
   );
 };
