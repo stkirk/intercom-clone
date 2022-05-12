6a04fe3ef9984da86a23ce121c21097a6e9728b0
stkirk <shaanekirk@gmail.com>
2022-05-12 11:04:05 -0400

    added name and value attributes to buttons for data control, initialized chatMessage state

--- src/components/SupportChatBot/ChatMessage.js
+++ src/components/SupportChatBot/ChatMessage.js
@@ -1,10 +1,36 @@
 import React from "react";
 
 const ChatMessage = () => {
+  const handleSelection = (e) => {
+    console.log(e.target.name, e.target.value);
+  };
   return (
     <div>
       <p>bot message</p>
-      <button>user response</button>
+      <button
+        className="userResponsebtn"
+        name="response 1"
+        value="1"
+        onClick={(e) => handleSelection(e)}
+      >
+        user response 1
+      </button>
+      <button
+        className="userResponsebtn"
+        name="response 2"
+        value="2"
+        onClick={(e) => handleSelection(e)}
+      >
+        user response 2
+      </button>
+      <button
+        className="userResponsebtn"
+        name="response 3"
+        value="3"
+        onClick={(e) => handleSelection(e)}
+      >
+        user response 3
+      </button>
     </div>
   );
 };
--- src/components/SupportChatBot/ChatMessageEngine.js
+++ src/components/SupportChatBot/ChatMessageEngine.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import ChatMessage from "./ChatMessage";
 
 const ChatMessageEngine = () => {
+  // chat message flow data controlled here
+  const [chatMessages, setChatMessages] = useState([]);
   return (
     <div>
       <ChatMessage />
